c2cb28fe88dbba1c7150e328cf8ea471e348d531
natsci-droid <natscidroid@gmail.com>
2020-05-16 21:01:47 +0100

    Added comments on hyperparameters

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -16,9 +16,9 @@ TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor 
 LR_CRITIC = 1e-3        # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
-UPDATE_EVERY = 1       # how often to update the network
-EPSILON = 1
-EPS_DECAY = 1e-4
+UPDATE_EVERY = 1        # how often to update the network
+EPSILON = 1             # how much to scale the noise
+EPS_DECAY = 1e-4        # how much to decay epsilon
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
